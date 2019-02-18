db8c1dd6b0e43a7e1754ed36db7c71741b9e22a7
Przepiorka <przepp3@medtronic.com>
2019-02-18 15:42:45 +0100

    Added addRemaining method

--- sorting-algorithms/src/MergeSortNew.java
+++ sorting-algorithms/src/MergeSortNew.java
@@ -3,9 +3,12 @@ import java.util.List;
 
 public class MergeSortNew {
 	public List<Integer> addRemaining(List<Integer> sortedList, Iterator<Integer> iter) {
+		int element;
+		while (iter.hasNext()) {
+			element = iter.next();
+			sortedList.add(element);
+		}
 		
-		
-		
-		return null;
+		return sortedList;
 	}
 }
