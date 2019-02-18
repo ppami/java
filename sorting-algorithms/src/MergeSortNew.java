869b5455e9d3140192afe2248805701b69519ef9
Przepiorka <przepp3@medtronic.com>
2019-02-18 15:38:42 +0100

    Added tests for addRemaining

--- sorting-algorithms/src/MergeSortNew.java
+++ sorting-algorithms/src/MergeSortNew.java
@@ -1,4 +1,11 @@
+import java.util.Iterator;
+import java.util.List;
 
 public class MergeSortNew {
-
+	public List<Integer> addRemaining(List<Integer> sortedList, Iterator<Integer> iter) {
+		
+		
+		
+		return null;
+	}
 }
--- sorting-algorithms/src/MergeSortNewTest.java
+++ sorting-algorithms/src/MergeSortNewTest.java
@@ -1,4 +1,39 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 
 public class MergeSortNewTest {
-
+	
+	@Test
+	void when_IterHasElementsAddRemaining() {
+		List<Integer> sortedList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+		List<Integer> iterList = new ArrayList<>(Arrays.asList(7, 8));
+		Iterator<Integer> iter = iterList.iterator();
+		
+		List<Integer> expected = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 7, 8));
+		
+		MergeSortNew msn = new MergeSortNew();
+		
+		assertEquals(expected, msn.addRemaining(sortedList, iter));
+		
+	}	
+	
+	@Test
+	void when_IterHasNoElementsAddRemaining() {
+		List<Integer> sortedList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+		List<Integer> iterList = new ArrayList<>();
+		Iterator<Integer> iter = iterList.iterator();
+		
+		List<Integer> expected = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+		
+		MergeSortNew msn = new MergeSortNew();
+		
+		assertEquals(expected, msn.addRemaining(sortedList, iter));
+		
+	}
 }
