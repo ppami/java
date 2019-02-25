5f52c496bb560b63b849e579c8248269863aaa65
ppami <przepiorka.pamela@gmail.com>
2019-02-25 18:04:55 +0100

    Added test for findSmallest() method

--- sorting-algorithms/src/SelectionSortTest.java
+++ sorting-algorithms/src/SelectionSortTest.java
@@ -1,4 +1,20 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 
 public class SelectionSortTest {
-
+	@Test
+	void given_IndexFindSmallestElement() {
+		List<Integer> list = new LinkedList<>(Arrays.asList(5, 3, 4, 6, 1, 2));
+		int index = 0;
+		int expected = list.get(4);
+		
+		SelectionSort ss = new SelectionSort();
+		assertEquals(expected, ss.findSmallest(list, index);
+	}
+	
 }
