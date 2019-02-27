3bcbabf001d412c1d8b40f7964d85a041352fd40
ppami <przepiorka.pamela@gmail.com>
2019-02-27 09:58:12 +0100

    changed findSmallest() method

--- sorting-algorithms/src/SelectionSort.java
+++ sorting-algorithms/src/SelectionSort.java
@@ -1,4 +1,17 @@
+import java.util.List;
 
 public class SelectionSort {
+	
+	public int findSmallest(List<Integer> list, int index) {
+		int smallest = index;
 
+		for (int i = index; i < list.size() - index; i++) {
+			if (list.get(i) < list.get(smallest)) {
+				smallest = i;
+			}
+		}
+		
+		
+		return smallest;
+	}
 }
--- sorting-algorithms/src/SelectionSortTest.java
+++ sorting-algorithms/src/SelectionSortTest.java
@@ -11,10 +11,10 @@ public class SelectionSortTest {
 	void given_IndexFindSmallestElement() {
 		List<Integer> list = new LinkedList<>(Arrays.asList(5, 3, 4, 6, 1, 2));
 		int index = 0;
-		int expected = list.get(4);
+		int expected = 4;
 		
 		SelectionSort ss = new SelectionSort();
-		assertEquals(expected, ss.findSmallest(list, index);
+		assertEquals(expected, ss.findSmallest(list, index));
 	}
 	
 }
