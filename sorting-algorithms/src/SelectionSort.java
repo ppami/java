844ffadd994cda7142f7031e414d234ee5d75aad
ppami <przepiorka.pamela@gmail.com>
2019-02-27 10:14:57 +0100

    fixed findSmallest() method

--- sorting-algorithms/src/SelectionSort.java
+++ sorting-algorithms/src/SelectionSort.java
@@ -16,7 +16,7 @@ public class SelectionSort {
 	public int findSmallest(List<Integer> list, int index) {
 		int smallest = index;
 
-		for (int i = index; i < list.size() - index; i++) {
+		for (int i = index; i < list.size(); i++) {
 			if (list.get(i) < list.get(smallest)) {
 				smallest = i;
 			}
